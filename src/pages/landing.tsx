354165e785982cd2a629025c603a3b71ae41269d
HARI-PRMD <haripramodb@gmail.com>
2023-03-27 11:07:42 +1100

    completed quote section

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -50,18 +50,6 @@ const Landing: NextPage = () => {
           <div className="mx-auto space-y-8 text-center">
             <h1>Our Features</h1>
             <div className="grid grid-cols-3 grid-rows-2 gap-8">
-              <button className="h-fit w-full space-y-4 rounded-lg border-2 border-opacity-20 p-8 text-left shadow-default duration-75 hover:shadow-default-solid">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="64"
-                  viewBox="0 96 960 960"
-                  width="64"
-                >
-                  <path d="M480 575q-66 0-108-42t-42-108q0-66 42-108t108-42q66 0 108 42t42 108q0 66-42 108t-108 42ZM160 896v-94q0-38 19-65t49-41q67-30 128.5-45T480 636q62 0 123 15.5t127.921 44.694q31.301 14.126 50.19 40.966Q800 764 800 802v94H160Zm60-60h520v-34q0-16-9.5-30.5T707 750q-64-31-117-42.5T480 696q-57 0-111 11.5T252 750q-14 7-23 21.5t-9 30.5v34Zm260-321q39 0 64.5-25.5T570 425q0-39-25.5-64.5T480 335q-39 0-64.5 25.5T390 425q0 39 25.5 64.5T480 515Zm0-90Zm0 411Z" />
-                </svg>
-                <h4>Find Fellow Peers to help with your courses</h4>
-                <p>idk bro</p>
-              </button>
               <button className="h-fit w-full space-y-4 rounded-lg border-2 border-opacity-20 p-8 text-left shadow-default hover:shadow-default-solid">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -122,6 +110,45 @@ const Landing: NextPage = () => {
                 <h4>Find Fellow Peers to help with your courses</h4>
                 <p>idk bro</p>
               </button>
+              <button className="h-fit w-full space-y-4 rounded-lg border-2 border-opacity-20 p-8 text-left shadow-default hover:shadow-default-solid">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="64"
+                  viewBox="0 96 960 960"
+                  width="64"
+                >
+                  <path d="M480 575q-66 0-108-42t-42-108q0-66 42-108t108-42q66 0 108 42t42 108q0 66-42 108t-108 42ZM160 896v-94q0-38 19-65t49-41q67-30 128.5-45T480 636q62 0 123 15.5t127.921 44.694q31.301 14.126 50.19 40.966Q800 764 800 802v94H160Zm60-60h520v-34q0-16-9.5-30.5T707 750q-64-31-117-42.5T480 696q-57 0-111 11.5T252 750q-14 7-23 21.5t-9 30.5v34Zm260-321q39 0 64.5-25.5T570 425q0-39-25.5-64.5T480 335q-39 0-64.5 25.5T390 425q0 39 25.5 64.5T480 515Zm0-90Zm0 411Z" />
+                </svg>
+                <h4>Find Fellow Peers to help with your courses</h4>
+                <p>idk bro</p>
+              </button>
+            </div>
+          </div>
+        </section>
+        <section className="mx-16 flex h-screen flex-col justify-center">
+          <div className="mx-auto space-y-16 text-center">
+            <h1>What Students are Saying</h1>
+            <div className="grid h-full max-w-screen-lg grid-cols-3 grid-rows-1">
+              <div className="-z-50 flex h-full w-fit -rotate-3 flex-col justify-between rounded-lg border-2 bg-background p-8 shadow-default">
+                <h4>
+                  &quot;I think this is kinda illegal but uhhhhh anyways
+                  gaslight gatekeep girlboss&quot;
+                </h4>
+                <p>Praneel, 2420</p>
+              </div>
+              <div className="flex h-full w-fit flex-col justify-between rounded-lg border-2 bg-background p-8 shadow-default">
+                <h4>
+                  &quot;This app helped me boost my WAM from 0 to 69&quot;
+                </h4>
+                <p>Michael, 2023</p>
+              </div>
+              <div className="-z-50 flex h-full w-fit rotate-3 flex-col justify-between rounded-lg border-2 bg-background p-8 shadow-default">
+                <h4>
+                  &quot;Made me from #ZeroWamNerd to #AcademicWeapon69 fr
+                  fr&quot;
+                </h4>
+                <p>Eoin, 2069</p>
+              </div>
             </div>
           </div>
         </section>
