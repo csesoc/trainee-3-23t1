578c652895cd432990704d0ad3967c20b10a1be0
Michael Girikallo <michael.girikallo@gmail.com>
2023-05-26 01:01:06 +0800

    Improved on styling closer to Figma

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -4,37 +4,23 @@ import Layout from "~/components/Layout";
 const Search: NextPage = () => {
   return (
     <Layout>
-      <div className="flex h-screen">
-        <div className="pt-10 ml-10 mr-10 w-[1000px] h-full fill-background">
-          <SearchBar/>
-          {/* Results info and results count info..
-              How to make it display actual values? */}
-          <div className="flex justify-between pt-5 pb-5">
-            <p>Results for 'COMP'</p>
-            <p>69 Results</p>
-          </div>
-            <CourseList/>
+      <div className="flex justify-between h-screen bg-secondary">
+        <div className="pt-10 ml-10 mr-10 w-[1000px] h-full bg-background pr-[40px] pl-[40px]">
+          <SearchBar/>   
+          <ReturnSearchList/>
         </div>
-        {/* Filter Section */}
-        <div className="pl-10 pt-10">
-          <h3 className="text-lg">Filter</h3>
-          <div className="flex">
-            <button className="flex items-center justify-center w-24 h-10 rounded-md bg-blue-500 hover:bg-red-500 text-white mr-2">
-              &#128214; Courses
-            </button>
-            <button className="flex items-center justify-center w-24 h-10 rounded-md bg-blue-500 hover:bg-red-500 text-white">
-              &#128100; Users
-            </button>
-          </div>
-        </div>
-      </div>
-      
+        <FilterSection/>
+      </div>  
     </Layout>
   );
 };
+
 // SearchBar Component
 const SearchBar = () => {
+  let search = "COMP"
+  let numResult = 691
   return (
+  <div>
   <form>   
       <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
       <div className="relative w-full bg-black">
@@ -45,10 +31,16 @@ const SearchBar = () => {
           <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
       </div>
   </form>
+  {/* Results thing beneath the search bar.. How to implement */}
+  <div className="flex justify-between pt-5 pb-5 ">
+  <p>Results for '{search}'</p>
+  <p>{numResult} Results</p>
+  </div>
+  </div>
   )
 }
-
-const CourseList = () => {
+// ResultsComponents
+const ReturnSearchList = () => {
   const data = [
     {
       name: "COMP1511",
@@ -82,7 +74,7 @@ const CourseList = () => {
     <div className="flex flex-wrap justify-between">
       {data.map((course, index) => {
         return (
-          <div key={index} className="flex w-[450px] h-[112px] bg-gray-200 p-4 mb-4 border border-blue-500 rounded-[10px]">
+          <div key={index} className="flex w-[450px] h-[112px] bg-gray-200 p-4 mb-4 rounded-[10px] shadow-default border-white bg-white/40">
             <img src={course.icon} alt="Course Icon" className="w-[80px] h-[80px] rounded-[10px] self-center" />
             <div className="ml-[16px]">
               <h5>{course.name}</h5>
@@ -95,5 +87,23 @@ const CourseList = () => {
   );
 }  
 
+const FilterSection = () => {
+return (
+<div className="pl-10 pt-10 h-screen bg-background w-[355px]">
+  <h3 className="text-sm">FILTER</h3>
+  <div className="flex flex-col">
+    <button className="mt-[10px] hover:border-accent-1 hover:text-accent-1 text-white font-bold py-2 px-4 h-[56px] w-[258px] rounded-[10px] flex items-center shadow-default border bg-white/40">
+      <img src="https://img.uxwing.com/wp-content/themes/uxwing/download/editing-user-action/read-book-icon.png" alt="Course Icon" className="w-4 h-4 mr-2"/>
+      <span>Courses</span>
+    </button>
+    <button className="mt-[10px] hover:border-accent-1 hover:text-accent-1 text-white font-bold py-2 px-4 h-[56px] w-[258px] rounded-[10px] flex items-center shadow-default border bg-white/40">
+      <img src="https://icons.veryicon.com/png/o/internet--web/prejudice/user-128.png" alt="User Icon" className="w-4 h-4 mr-2"/>
+      <span>Users</span>
+    </button>
+  </div>
+</div>
+)
+
+}
 
 export default Search;
